682bbcf6f6c12fc3b4babf361d7a8a31b8f7a0b5
debugwith-Vaishnavi <vaishnavishinde8795@gmail.com>
2026-06-18 23:07:47 +0530

    Fix api urls

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,10 @@
-# api/urls.py
 from django.urls import path
 from .views import register_view, login_view, profile_view, predict_view, history_view
 
-
 urlpatterns = [
-    path('result/', lambda request: render(request, 'result.html')),
-path('profile/', lambda request: render(request, 'profile.html')),
-path('history/', lambda request: render(request, 'history.html')),
-path('predict/', lambda request: render(request, 'predict.html')),
-path('dashboard/', lambda request: render(request, 'dashboard.html')),
-path('register/', lambda request: render(request, 'register.html')),
+    path('register/', register_view),
+    path('login/', login_view),
+    path('profile/', profile_view),
+    path('predict/', predict_view),
+    path('history/', history_view),
 ]
